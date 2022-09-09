e39bb270b964bad932fa239b46dfea1065039720
NadineSafwat <nadinhassann@yahoo.com>
2022-09-09 15:11:12 -0700

    alphabets again

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always returns 0
+ *
+ */
+
+int main(void)
+{
+	int i;
+	char alps[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHILKLMNOPQRSTUVWXYZ"
+
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alps[i]);
+	}
+	putchar("\n");
+	return (0);
+}
