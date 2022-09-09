e62832557832aa32961f66814237510d35e909bd
NadineSafwat <nadinhassann@yahoo.com>
2022-09-09 15:48:36 -0700

    print numbers with getchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always return 0
+ *
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
