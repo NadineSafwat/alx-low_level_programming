b884d13abc7f1a47e429a4ccbb4902ebc8ee8a0d
NadineSafwat <nadinhassann@yahoo.com>
2022-09-13 07:56:23 -0700

    prints alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Return: always zero when success
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n')
+	return (0);
