4bf6a55636ccf0dec3c1f501a1c7dbfb1ad05f34
NadineSafwat <nadinhassann@yahoo.com>
2022-09-13 12:07:03 -0700

    last task

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints from any number to 98
+ *
+ * @n: the number to start from
+ *
+ * Return: void
+ */
+
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		for (; n > 98; n--)
+		{
+			printf("%d, ", n);
+		}
+	}
+	else if (n < 98)
+	{
+		for (; n < 98; n++)
+		{
+			printf("%d, ", n);
+		}
+	}
+}
