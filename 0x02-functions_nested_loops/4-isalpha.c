033df588d5c35760710d5846998cca6746bcb69a
NadineSafwat <nadinhassann@yahoo.com>
2022-09-13 08:40:01 -0700

    fun check alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if char is alphabet
+ *
+ * @c: The character in ASCII code
+ *
+ * Return: returns 1 if alphabet 0 otherwise
+ *
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 97 && c <123 || c >= 65 && c < 91)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
