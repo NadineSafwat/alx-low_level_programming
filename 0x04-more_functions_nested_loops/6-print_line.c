2b816f049725bef661a95107e2522105a8404ec9
NadineSafwat <nadinhassann@yahoo.com>
2022-09-15 15:44:11 -0700

    working function

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - pring series of underscore
+ * @n: the number of underscores to be printed
+ * Return: nothing(void)
+ */
+
+void print_line(int n)
+{
+	int i = 0;
+
+	while (i <= n)
+	{
+		_putchar(95);
+		i++;
+	}
+	_putchar('\n');
+}
