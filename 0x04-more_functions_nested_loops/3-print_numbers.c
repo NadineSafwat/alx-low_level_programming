cb3be1a5b64ad86be3bc4fd1bc92d6d200f38cd0
NadineSafwat <nadinhassann@yahoo.com>
2022-09-15 10:52:21 -0700

    print numbers function

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints out numbers from 0 to 9
+ *
+ * Return: nothing
+ */
+
+void print_numbers(void)
+{
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+}
