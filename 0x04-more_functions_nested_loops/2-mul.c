22c5522993390d0315774ddf7390fa11a0c6a89a
NadineSafwat <nadinhassann@yahoo.com>
2022-09-15 10:48:09 -0700

    mutiplication function

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiply its two parameter.
+ * @a: the first paremeter if type int
+ * @b: the second parameter of type int
+ * Return: the result of multiplication of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
