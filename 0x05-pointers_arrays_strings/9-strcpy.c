90577a14002214da57a1aa424de9304a89194028
NadineSafwat <nadinhassann@yahoo.com>
2022-09-19 17:00:14 -0700

    last task

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - copy a string to another
+ * @dest: destination
+ * @src: parameter with string
+ * Return: copied string
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		*(dest + count) = *(src + count);
+		if (*(src + count) == '\0')
+			break;
+		count++;
+	}
+	return (dest);
+}
