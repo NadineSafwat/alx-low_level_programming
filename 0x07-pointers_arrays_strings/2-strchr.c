f8d181ba44c3fde7468e82dd2bf063e2cc51f73a
NadineSafwat <nadinhassann@yahoo.com>
2022-09-26 15:00:09 -0700

    2-strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strchr - searches for a charecter in a string
+ * @s: pointer to a string
+ * @c: charecter to be searched for
+ * Return: pointer to the first occur of c or NULL is not found
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) == c)
+		{
+			return (s + i);
+		}
+		i++;
+	}
+	return ('\0');
+}
