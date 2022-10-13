68eabdacc0be794f2ce73365889f16ae997988d3
NadineSafwat <nadinhassann@yahoo.com>
2022-10-13 08:49:34 -0700

    fixing some issues

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,12 +16,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(plist, n);
 
-	for (i == 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(plist, int));
 		if (separator != '\0' && i < n - 1)
 		{
-			printf("%c", separator);
+			printf("%s", separator);
 		}
 	}
 
