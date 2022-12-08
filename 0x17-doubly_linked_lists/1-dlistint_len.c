9843cdfbd96d739ec5c505cc233e3f7da8362437
NadineSafwat <nadinhassann@yahoo.com>
2022-12-08 06:19:07 -0800

    firt two task

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - returns number of elements in DLL
+ *
+ * @h: head of the list
+ * Return: the number of nodes
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	int num;
+
+	num = 0;
+
+	if (h == NULL)
+	{
+		return (num);
+	}
+	while (h->prev != NULL)
+	{
+		h = h->prev;
+	}
+	while (h != NULL)
+	{
+		num++;
+		h = h->next;
+	}
+	return (num);
+}
